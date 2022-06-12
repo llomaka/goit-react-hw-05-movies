6974676023d89f31c9ace8ad0addac1198beb580
Lilia Lomaka <llomaka80@gmail.com>
2022-06-12 16:17:13 +0300

    Тестування меню з маршрутизацією

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,25 +1,12 @@
-import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './NavigationBar.module.css';
 
 export default function NavigationBar() {
-  const [activeMenu, setActiveMenu] = useState('home');
-  const menuClick = event => {
-    event.preventDefault();
-    setActiveMenu(event.target.id);
-  };
-  const homeStyle = { color: activeMenu === 'home' ? '#FD8A2A' : null};
-  const moviesStyle = {color: activeMenu === 'movies' ? '#FD8A2A' : null};
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li id='home' className={styles.item} style={homeStyle} onClick={menuClick}>
-              Home
-          </li>
-          <li id='movies'  className={styles.item} style={moviesStyle} onClick={menuClick}>
-              Movies
-          </li>
-        </ul>
+        <NavLink exact to='/' className={styles.link} activeClassName={styles.active_link}>Home</NavLink>
+        <NavLink to='/movies'  className={styles.link} activeClassName={styles.active_link}>Movies</NavLink>
       </nav>
     </header>
   )
