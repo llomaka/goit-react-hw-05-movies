feedb2335137305984a75f6b806d44a90ad5d658
Lilia Lomaka <llomaka80@gmail.com>
2022-06-16 20:43:06 +0300

    Доопрацювання сторінки подробиць

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,64 @@
+import emptyPoster from './no-poster.svg';
+import { fetchMovieById } from 'service/api';
+import { useState, useEffect } from 'react';
+import { RemoveScroll } from 'react-remove-scroll';
+import Modal from 'components/Modal';
+import useModal from '../../hooks/useModal';
+import { Link, useParams } from 'react-router-dom';
+import styles from './MovieDetails.module.css';
+import Loader from 'components/Loader';
+
+export default function MovieDetails() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const { posterPath, altCaption, closeModal, selectImage } = useModal();
+
+  useEffect(() => {
+    setLoader(true);
+    fetchMovieById(movieId)
+      .then(setMovie)
+      .catch(setError)
+      .finally(setLoader(false));
+  }, [movieId]);
+
+  return (
+    < section className={styles.section} >
+      {loader && <Loader />}
+      {error && <p>Something went wrong. Error message: {error.message}.</p>}
+      {movie && (<>
+        <div className={styles.card}>
+          <img
+            className={styles.image}
+            src={movie.poster_path ? `https://image.tmdb.org/t/p/w342${movie.poster_path}` : emptyPoster}
+            alt={`${movie.title} Poster`}
+            loading="lazy"
+            onClick={() => selectImage(`https://image.tmdb.org/t/p/original${movie.poster_path}`, movie.title)}
+          />
+          <div className={styles.caption}>
+            <h2 className={styles.title}>{movie.title} ({movie.release_date ? movie.release_date.slice(0, 4) : 'No Release Year Available'})</h2>
+            <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
+            <h3>Overview</h3>
+            <p>{movie.overview ? movie.overview : 'No Overview Available'}</p>
+            <h3>Genres</h3>
+            <p>{movie.genres ? movie.genres.map(genre => genre.name).join(', ') : 'No Genres Available'}</p>
+          </div>
+        </div>
+        <div className={styles.aside}>
+          <h2 className={styles.aside_title}>Additional information</h2>
+          <ul className={styles.list}>
+            <li className={styles.item}><Link to={'cast'}>Cast</Link></li>
+            <li className={styles.item}><Link to={'reviews'}>Reviews</Link></li>
+          </ul>
+        </div></>)}
+      {posterPath && movie.poster_path && <RemoveScroll>
+        <Modal
+          posterPath={posterPath}
+          altCaption={altCaption}
+          closeModal={closeModal}
+        />
+      </RemoveScroll>}
+    </section >
+  );
+}
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,70 +1,16 @@
-import emptyPoster from './no-poster.svg';
-import { fetchMovieById } from 'service/api';
-import { useState, useEffect } from 'react';
-import { RemoveScroll } from 'react-remove-scroll';
-import Modal from 'components/Modal';
-import useModal from '../hooks/useModal';
-import { Outlet, Link, useNavigate, useParams } from 'react-router-dom';
-import styles from './MovieDetailsPage.module.css';
-import Loader from 'components/Loader';
+import { Outlet, Link, useNavigate, useLocation } from 'react-router-dom';
+import MovieDetails from 'components/MovieDetails';
 
 export default function MovieDetailsPage() {
-  const { movieId } = useParams();
-  const [movie, setMovie] = useState(null);
-  const [error, setError] = useState(null);
-  const [loader, setLoader] = useState(false);
+  const location = useLocation();
   const navigate = useNavigate();
-  const { posterPath, altCaption, closeModal, selectImage } = useModal();
-
-  useEffect(() => {
-    setLoader(true);
-    fetchMovieById(movieId)
-      .then(setMovie)
-      .catch(setError)
-      .finally(setLoader(false));
-  }, [movieId]);
-
-  function goBackNavigation() {
-    navigate(-1);
-  }
-
+  console.dir(location);
   return (
-    < section className={styles.section} >
-      {loader && <Loader />}
-      {error && <p>Something went wrong. Error message: {error.message}.</p>}
-      {movie && (<><button className={styles.button} type='button' onClick={goBackNavigation}>Go back</button>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src={movie.poster_path ? `https://image.tmdb.org/t/p/w342${movie.poster_path}` : emptyPoster}
-            alt={`${movie.title} Poster`}
-            loading="lazy"
-            onClick={() => selectImage(`https://image.tmdb.org/t/p/original${movie.poster_path}`, movie.title)}
-          />
-          <div className={styles.caption}>
-            <h2 className={styles.title}>{movie.title} ({movie.release_date ? movie.release_date.slice(0, 4) : 'No Release Year Available'})</h2>
-            <p>User Score: {Math.round(movie.vote_average * 10)}%</p>
-            <h3>Overview</h3>
-            <p>{movie.overview ? movie.overview : 'No Overview Available'}</p>
-            <h3>Genres</h3>
-            <p>{movie.genres ? movie.genres.map(genre => genre.name).join(', ') : 'No Genres Available'}</p>
-          </div>
-        </div>
-        <div className={styles.aside}>
-          <h2 className={styles.aside_title}>Additional information</h2>
-          <ul className={styles.list}>
-            <li className={styles.item}><Link to={'cast'}>Cast</Link></li>
-            <li className={styles.item}><Link to={'reviews'}>Reviews</Link></li>
-          </ul>
-        </div></>)}
+    <>
+      <button type='button' onClick={()=>navigate(-1)}>Go Back</button>
+      <Link to='..'>Go Up</Link>
+      <MovieDetails />
       <Outlet />
-      {posterPath && movie.poster_path && <RemoveScroll>
-        <Modal
-          posterPath={posterPath}
-          altCaption={altCaption}
-          closeModal={closeModal}
-        />
-      </RemoveScroll>}
-    </section >
+    </>
   );
 }
