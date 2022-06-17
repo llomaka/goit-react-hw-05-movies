ebfd83a11b5fc00d458b6647ea304dc0ccecfc57
Lilia Lomaka <llomaka80@gmail.com>
2022-06-17 21:48:26 +0300

    Перевірка адресного рядка на нечислове значеня ідентифікатора фільму

--- src/hooks/useFetchMovie.js
+++ src/hooks/useFetchMovie.js
@@ -1,22 +1,26 @@
 import { fetchMovieById } from 'service/api';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function useFetchMovie() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
   const isPrevPage = window.history.state.idx;
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchMovieById(movieId)
-      .then(setMovie)
-      .catch(setError)
-      .finally(setIsLoading(false));
-  }, [movieId]);
+    if (isNaN(movieId)) {
+      navigate('/');
+    } else {
+      setIsLoading(true);
+      fetchMovieById(movieId)
+        .then(setMovie)
+        .catch(setError)
+        .finally(setIsLoading(false));
+    }
+  }, [movieId, navigate]);
 
   return { movie, error, isLoading, isPrevPage };
 }
